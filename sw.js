158c7c09e7d343270431265b1e3ed03b71301599
Fabio Tovar <fstovarr@unal.edu.co>
2020-11-28 04:09:56 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-08f3e88eedef6d92bd86.js"
+    "url": "webpack-runtime-4ed6792689eb579d0896.js"
   },
   {
     "url": "framework-c8d3a5345e3f321d7da5.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d6661fcbf07556286505031bd472b7f6"
+    "revision": "08ba243a6f9c41985bea620c366b6a09"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a95b272f8c25b9a8a758.js"
